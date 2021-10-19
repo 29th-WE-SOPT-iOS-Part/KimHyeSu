08ac703adba4ad89509cf5442a624856ea35c537
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-10-20 02:33:16 +0900

    ✨[FEAT] 탭바 투명하도록 설정 (#9)

--- Youtube-Clone/Youtube-Clone/Source/MainTab/MainTabVC.swift
+++ Youtube-Clone/Youtube-Clone/Source/MainTab/MainTabVC.swift
@@ -9,7 +9,17 @@ import UIKit
 
 class MainTabVC: UITabBarController {
 
+    // MARK: - VC LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTabbarClear()
+    }
+    
+    // MARK: - UI Setting
+    func setTabbarClear() {
+        self.tabBar.layer.borderWidth = 0.50
+        self.tabBar.layer.borderColor = UIColor.clear.cgColor
+        self.tabBar.clipsToBounds = true
+        self.tabBar.backgroundImage = UIImage()
     }
 }
