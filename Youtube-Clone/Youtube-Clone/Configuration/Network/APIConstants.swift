a47483a4263ba05917caa90498ce2df4b6b21f72
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-11-11 17:48:10 +0900

    ➕[ADD] Alamofire 추가 (#13)

---
+++ Youtube-Clone/Youtube-Clone/Configuration/Network/APIConstants.swift
@@ -0,0 +1,18 @@
+//
+//  APIConstants.swift
+//  Youtube-Clone
+//
+//  Created by 김혜수 on 2021/11/11.
+//
+
+import Foundation
+
+struct APIConstants {
+    
+    // MARK: - Base URL
+    static let baseURL = "https://asia-northeast3-we-sopt-29.cloudfunctions.net/api"
+    
+    // MARK: - /user
+    static let loginURL = "/user/login"
+    
+}
